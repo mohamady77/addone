adbad4995e4120d22b7e4079f3e4890902be75c0
mohamady77 <34239842+mohamady77@users.noreply.github.com>
2017-12-04 16:50:12 +0330

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+	plug = {
+    "TD"
+  },
+  Helper = "HelperEagleboT",
+  Token = "412174902:AAGA4EzGr3Ct7hobFoGzGBnQzC-XIXUzshQ",
+  bot_owner = {
+    484131026,
+    499929818
+  },
+  cmd = "^[/!#]",
+  sudo_users = {
+    484131026
+  }
+}
+return _
+end
